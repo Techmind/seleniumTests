3771347d5d28374c7dd700e32947bd0f056c866f
Bogunov Ilya <bogunov@gmail.com>
2013-02-17 13:53:28 +0400

    gitignore  + sh selenium test

--- init.sh
+++ init.sh
@@ -2,5 +2,6 @@
 
 mkdir -p .mozilla/firefox/
 cp -R .mozilla/firefox/ $HOME/.mozilla/firefox
+ls -la selenium-server-standalone-2.29.0.jar || wget http://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar
 
 nohup java -jar selenium-server-standalone-2.29.0.jar -Dwebdriver.firefox.profile="TESTING" 2>&1 > /dev/null &
