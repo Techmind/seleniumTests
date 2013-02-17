806dcdcdeefe2682feb69c0b4b06943f0a36cb67
Bogunov Ilya <bogunov@gmail.com>
2013-02-17 14:16:10 +0400

    make correct profiel copy

--- init.sh
+++ init.sh
@@ -1,7 +1,8 @@
 #/usr/bin/sh
 
+firefox -CreateProfile TESTING
 mkdir -p .mozilla/firefox/
-cp -R .mozilla/firefox/*TESTING $HOME/.mozilla/firefox
+cp -R .mozilla/firefox/*TESTING/ $HOME/.mozilla/firefox/*TESTING/
 ls -la selenium-server-standalone-2.29.0.jar || wget http://selenium.googlecode.com/files/selenium-server-standalone-2.29.0.jar
 
 nohup java -jar selenium-server-standalone-2.29.0.jar -Dwebdriver.firefox.profile="TESTING" 2>&1 > /dev/null &
